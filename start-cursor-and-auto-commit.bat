d9817dddbe5b41aaaaad83dd959c28eec8da2b97
Il Tuo Nome <tua-email@gmail.com>
2025-06-15 05:57:28 +0200

    auto-commit

--- start-cursor-and-auto-commit.bat
+++ start-cursor-and-auto-commit.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-start "" "C:\Users\Filippo Pizzorni\AppData\Local\Programs\cursor\Cursor.exe"
+start "" "C:\Users\filpi\AppData\Local\Programs\cursor\Cursor.exe"
 start "" cmd /k "auto-commit-push.bat" 
