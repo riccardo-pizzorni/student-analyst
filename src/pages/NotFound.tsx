5c7ca4d9c9feb4ab2c7cc43b2ea0a22baeb581a1
Il Tuo Nome <tua-email@gmail.com>
2025-06-16 19:52:20 +0200

    auto-commit

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,1 +1,5 @@
- 
+import React from "react";
+
+export default function NotFound() {
+  return <div style={{ padding: 40, textAlign: 'center' }}><h1>404 - Pagina non trovata</h1><p>La pagina che cerchi non esiste.</p></div>;
+} 
