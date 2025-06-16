93d8023988667a16db02f5720511d3e2466637ed
Il Tuo Nome <tua-email@gmail.com>
2025-06-16 17:26:44 +0200

    auto-commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,5 @@
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { monitoring } from './utils/monitoring'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-// Initialize monitoring system
-monitoring.init()
-
-// Professional error handling for the entry point
-const rootElement = document.getElementById('root')
-
-if (!rootElement) {
-  throw new Error('Root element not found. Make sure index.html has a div with id="root"')
-}
-
-// Initialize React application
-createRoot(rootElement).render(
-  <StrictMode>
-    <App />
-    <Analytics />
-    <SpeedInsights />
-  </StrictMode>,
-) 
+createRoot(document.getElementById("root")!).render(<App />); 
